6399b4b64856895cdd59d8bcefaf4df3c6d63c3e
DrSimaB <calvinbenatha@gmail.com>
2022-12-02 00:55:16 +0200

    Update 5-variable_load.py

--- 0x02-python-import_modules/5-variable_load.py
+++ 0x02-python-import_modules/5-variable_load.py
@@ -1,4 +1,10 @@
-#!/usr/bin/python
-from variable_load_5 import a
-if _name_ == "_main_":
-    print("{}".format(a))
+#!/usr/bin/python3
+# 5-variable_load.py
+
+if __name__ == "__main__":
+    """Print the value of variable a from variable_load_5."""
+    from variable_load_5 import a
+
+    print(a)
+Footer
+© 2022 GitHub, Inc.
