2f44d589ee9563bc7d432572cdd54f0c4777c192
DrSimaB <calvinbenatha@gmail.com>
2022-12-07 15:43:25 +0200

    Update 1-search_replace.py

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -3,10 +3,10 @@
 # Benatha Simasiku
 
 
-def search_replace(my_list, appearnace, replacer):
+def search_replace(my_list, search, replace):
     """Replace all occurrences of an element by another in a new list."""
     new_list = my_list[:]
     for i in range(len(new_list)):
-        if new_list[i] == appearnace: #2 at main file
-            new_list[i] = replacer #89 at main file
+        if new_list[i] == search:
+            new_list[i] = replace
     return (new_list)
