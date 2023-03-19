ccfb8b3abcdfb797f14af8b0c31bf4d8b48498ec
DrSimaB <calvinbenatha@gmail.com>
2023-03-19 02:33:13 +0200

    Update 7-model_state_fetch_all.py

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -15,15 +15,13 @@ if __name__ == "__main__":
     db = sys.argv[3]
     host = "localhost"
     port = 3306
+if __name__ == "__main__":
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
+     
+                          .format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                           pool_pre_ping=True)
+    Session = sessionmaker(bind=engine)
+    session = Session()
 
-    engine = create_engine("{}://{}:{}@{}:{}/{}".format(
-        dialect,
-        user,
-        passwd,
-        host,
-        port,
-        db
-    ))
-    result = engine.execute("SELECT * FROM states ORDER BY id")
-    for _r in result:
-        print("{}: {}".format(_r[0], _r[1]))
+    for state in session.query(State).order_by(State.id):
+        print("{}: {}".format(state.id, state.name))
