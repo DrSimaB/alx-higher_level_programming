74b18bb1e2f1a57639603b74ad70cf2e7e7c0f13
DrSimaB <calvinbenatha@gmail.com>
2024-01-04 01:22:23 +0200

    Update 0-readme.js

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,5 +1,8 @@
 #!/usr/bin/node
 const fs = require('fs');
-fs.readFile(process.argv[2], 'utf8', function (error, content) {
-  console.log(error || content);
+const args = process.argv.slice(2)[0];
+
+fs.readFile(args, 'utf-8', (err, data) => {
+  if (err) console.log(err);
+  else console.log(data);
 });
