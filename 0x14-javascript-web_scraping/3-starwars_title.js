53ea16a7edfdf7dc1f204c4f43cc2a3d00611426
DrSimaB <calvinbenatha@gmail.com>
2024-01-04 01:39:05 +0200

    Update 3-starwars_title.js

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,6 +1,17 @@
 #!/usr/bin/node
 const request = require('request');
-let url = 'http://swapi.co/api/films/' + process.argv[2];
-request(url, function (error, response, body) {
-  console.log(error || JSON.parse(body).title);
+const args = process.argv.slice(2);
+const id = args[0];
+
+const options = {
+  url: `https://swapi.co/api/films/${id}`,
+  method: 'GET'
+};
+
+request(options, (err, res, body) => {
+  if (err) console.log(err);
+  if (res) {
+    const data = JSON.parse(body);
+    console.log(data.title);
+  }
 });
