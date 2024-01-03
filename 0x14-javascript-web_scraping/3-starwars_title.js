99bea63550c118507a77aa98aaff90025743c781
DrSimaB <calvinbenatha@gmail.com>
2024-01-04 01:41:13 +0200

    Update 3-starwars_title.js

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -4,7 +4,7 @@ const args = process.argv.slice(2);
 const id = args[0];
 
 const options = {
-  url: `https://swapi.co/api/films/${id}`,
+  url: `https://swapi-api.alx-tools.com/api/films/:id`,
   method: 'GET'
 };
 
